3bd8212615ac07611a66963ec7bd234a5e741359
Daniel <dm858@york.ac.uk>
2019-12-01 21:56:27 +0000

    Added extra comments

--- 1/counter_upper.cpp
+++ 1/counter_upper.cpp
@@ -3,16 +3,20 @@
 #include<vector>
 #include<cstdlib>
 
+// forward function declarations
 std::vector<double> read_mass(std::string filename, std::vector<double>& masses);
 int fuel_for_module(int module_fuel);
 
 int main(){
 
+   // parameters
    std::vector<double> module_masses(0);
    int total_fuel = 0;
 
+   // read module masses
    read_mass("input", module_masses);
 
+   // calculate fuel required
    for ( int i=0; i < module_masses.size(); i++ ){
       total_fuel += fuel_for_module(module_masses[i]);
    }
@@ -41,13 +45,16 @@ std::vector<double> read_mass(std::string filename, std::vector<double>& masses)
 }
 
 // fuel for module calculator
+// reqursively works out the fuel required for the calculated fuel
 int fuel_for_module( int module_mass ){
 
    int fuel = ((int)((double)module_mass/3.0)) - 2;
 
+   // no fuel required for extra fuel
    if ( ((int)((double)fuel/3.0) - 2) <= 0 ){
       return fuel;
    }
+   // calculate fuel required for extra fuel
    else {
       return (fuel + fuel_for_module(fuel));
    }
