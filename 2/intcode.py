76778a7fcef8247544cbbaefb8b2cc32f6d2b21e
Daniel <dm858@york.ac.uk>
2019-12-02 16:35:33 +0000

    commented python code

--- 2/intcode.py
+++ 2/intcode.py
@@ -1,6 +1,7 @@
 import numpy as np
 import csv
 
+# calculate opcode
 def calculate(array,index):
 
     if array[index] == 1:
@@ -19,17 +20,21 @@ def calculate(array,index):
         print("opcode must be one of [1,2,99]")
     return 0
 
+# set array position 1 and 2
 def set_ab(a,b,array):
     array[1] = a
     array[2] = b
 
+# read csv into numpy array
 with open('input.txt') as file:
     reader = csv.reader(file)
     y = np.array(list(reader)[0],dtype=int)
 
+# value copy of y (NOT REFERENCE ugh python)
 y_save = np.copy(y)
 
 done = False
+# brute force answer
 for i in range(0,100,1):
     for j in range(0,100,1):
         set_ab(i,j,y)
