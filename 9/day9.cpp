049da8863d4902d2b3c676636cfba92f2141d2ff
Daniel Meilak <dm858@york.ac.uk>
2019-12-09 18:37:58 +0100

    finished day 9

--- 9/day9.cpp
+++ 9/day9.cpp
@@ -1,12 +1,21 @@
 #include<iostream>
+#include<chrono>
 #include"intcode.hpp"
 
 int main(){
 
-   computer_t computer("test_input", {});
+   // Record start time
+   auto start = std::chrono::high_resolution_clock::now();
+
+   computer_t computer("input", {2});
 
    computer.run();
 
-   std::cout << "output: " << computer.output << std::endl;
    std::cout << "status: " << computer.status << std::endl;
+
+   // Record end time
+   auto finish = std::chrono::high_resolution_clock::now();
+   std::chrono::duration<double> elapsed = finish - start;
+   std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+
 }
