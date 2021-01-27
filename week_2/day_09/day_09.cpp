99f61f7edb72c1092bed1067cab10262ee3a8427
Daniel <daniel.mlak@gmail.com>
2021-01-27 19:09:52 +0100

    Cleaned up day 9

---
+++ week_2/day_09/day_09.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<chrono>
+#include"intcode.h"
+#include"../../Utils/utils.h"
+
+int main(){
+
+   // read input into vector of strings.
+   std::vector<long long> input = input_to_llint(read_input("input", ","));
+
+   // part 1
+   computer_t comp(input, {1});
+   comp.run();
+   std::cout << "Answer (part 1): " << comp.output.back() << std::endl;
+
+   // part 2
+   comp.reset({2});
+   comp.run();
+   std::cout << "Answer (part 2): " << comp.output.back() << std::endl;
+}
