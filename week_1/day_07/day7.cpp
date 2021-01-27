b6acb4a173d2784dc57c41199c01f18e42544e97
Daniel <daniel.mlak@gmail.com>
2021-01-27 18:22:26 +0100

    Cleaned up day 7

--- week_1/day_07/day7.cpp
+++ week_1/day_07/day7.cpp
@@ -1,8 +1,8 @@
-
 #include<vector>
 #include<algorithm>
 #include<iostream>
-#include "intcode.hpp"
+#include"intcode.h"
+#include"../../Utils/utils.h"
 
 int main(){
 
@@ -11,14 +11,16 @@ int main(){
    int best_signal = 0;
    std::vector<int> best_phase;
 
-   // create a starting computer point, and an amplifier of 5 computers
-   computer_t computer("input", {0});
-   std::vector<computer_t> amplifier(5, computer);
+   // read input into vector of strings.
+   std::vector<int> input = input_to_int(read_input("input", ","));
+
+   // create an amplifier of 5 computers
+   std::vector<computer_t> amplifier(5, computer_t(input, {0}));
 
    do {
 
       // initialise computers with the correct phase
-      for (int i=0; i<phase.size(); i++){
+      for (size_t i=0; i<phase.size(); i++){
          amplifier[i].reset({phase[i]});
       }
 
@@ -26,7 +28,7 @@ int main(){
 
       // run amplifier grid for each phase value
       while ( amplifier[4].status != "finished" ){
-         for (int i=0; i<amplifier.size(); i++){
+         for (size_t i=0; i<amplifier.size(); i++){
             amplifier[i].input.push_back(output);
             amplifier[i].run();
             output = amplifier[i].output;
@@ -36,16 +38,12 @@ int main(){
       // save best outputs
       if ( output > best_signal){
          best_signal = output;
-         best_phase = phase;
+         best_phase  = phase;
       }
 
    } while (std::next_permutation(phase.begin(), phase.end()));
 
-   std::cout << "Max signal: " << best_signal << std::endl;
-   for (int i=0; i<best_phase.size(); i++){
-      std::cout << best_phase[i] << " ";
-   }
-   std::cout << std::endl;
+   std::cout << "Answer (part 2): " << best_signal << std::endl;
 
    return 0;
 }
