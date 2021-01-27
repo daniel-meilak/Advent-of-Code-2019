71a1bf8ed027e5b3494258325c58ec162f09f942
Daniel <daniel.mlak@gmail.com>
2021-01-27 18:06:02 +0100

    Cleaned up day 6

---
+++ day_13/day_13.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+//#include"../../Utils/utils.h"
+
+int main(){
+
+    // // read input into vector of strings.
+    // std::vector<std::string> input = read_input("input", "");
+
+    // // read input into vector of vector of strings.
+    // std::vector<std::string> delimiters = {};
+    // std::vector<std::vector<std::string>> input = read_input_2D("input", delimiters);
+
+    std::cout << "Answer (part 1): " << std::endl;
+    std::cout << "Answer (part 2): " << std::endl;
+
+    return 0;
+}
