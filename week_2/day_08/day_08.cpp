2f79a80e9585ca40e41e829d35b91f13ba405561
Daniel <daniel.mlak@gmail.com>
2021-01-27 18:29:51 +0100

    Changed day names and cleaned up day 8

--- week_2/day_08/day_08.cpp
+++ week_2/day_08/day_08.cpp
@@ -3,6 +3,7 @@
 #include<fstream>
 #include<cstdlib>
 
+//std::vector<std::vector<std::string>> read_data(filename);
 std::vector<std::string> read_data(std::string filename);
 
 int main(){
@@ -11,10 +12,33 @@ int main(){
 
    layers = read_data("input");
 
+   int min_0 = 100000;
+   int save_1, save_2;
+
+   for (size_t i=0; i<layers.size(); i++){
+      int num_0 = 0;
+      int num_1 = 0;
+      int num_2 = 0;
+
+      for (size_t j=0; j<layers[i].size(); j++){
+         if (layers[i][j] == '0'){ num_0++; }
+         else if (layers[i][j] == '1'){ num_1++; }
+         else if (layers[i][j] == '2'){ num_2++; }
+      }
+
+      if ( num_0 < min_0 ){
+         min_0  = num_0;
+         save_1 = num_1;
+         save_2 = num_2;
+      }
+   }
+
+   std::cout << "Answer (part 1): " << save_1*save_2 << std::endl;
+
    std::string picture;
 
-   for (int i=0; i<150; i++){
-      for (int j=0; j<layers.size(); j++){
+   for (size_t i=0; i<150; i++){
+      for (size_t j=0; j<layers.size(); j++){
 
          if (layers[j][i] == '0'){
             picture.push_back(' ');
@@ -27,8 +51,8 @@ int main(){
       }
    }
 
-   std::cout << "NOTE: Requires monospace font" << std::endl;
-   for (int i=0; i<picture.size(); i+=25){
+   std::cout << "Answer (part 2): " << std::endl;
+   for (size_t i=0; i<picture.size(); i+=25){
       std::cout << picture.substr(i,25) << std::endl;
    }
 
@@ -49,7 +73,7 @@ std::vector<std::string> read_data(std::string filename){
    std::string pixel;
    std::vector<std::string> layers;
 
-   for (int i=0; i<line.size();i+=150){
+   for (size_t i=0; i<line.size();i+=150){
       pixel = line.substr(i,150);
       layers.push_back(pixel);
    }
