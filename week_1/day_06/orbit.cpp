74097f486c56b018c14c5620e1d8476bf690cb9c
Daniel <daniel.mlak@gmail.com>
2021-01-27 18:06:02 +0100

    Cleaned up day 6

--- week_1/day_06/orbit.cpp
+++ week_1/day_06/orbit.cpp
@@ -4,6 +4,7 @@
 #include<fstream>
 #include<cstdlib>
 #include<algorithm>
+#include"../../Utils/utils.h"
 
 class orbit_t{
 public:
@@ -16,34 +17,33 @@ public:
 };
 
 orbit_t::orbit_t( std::string name, std::string linked_to ){
-   this->name = name;
+   this->name      = name;
    this->linked_to = linked_to;
    links = 1;
 }
 
 int find_planet_index( std::vector<orbit_t> system, std::string name );
-std::vector<std::string> read_input();
 
 int main(){
 
-   std::vector<std::string> map;
-   map = read_input();
+   // read input into vector of strings.
+   std::vector<std::string> map = read_input("input", ")");
 
    std::vector<orbit_t> system;
 
    // create vector of orbit objects
-   for (int i=1; i<map.size(); i+=2){
+   for (size_t i=1; i<map.size(); i+=2){
       orbit_t orbit( map[i], map[i-1] );
       system.push_back(orbit);
    }
 
    // find the index of each planet for later calc.
-   for (int i=0; i<system.size(); i++){
+   for (size_t i=0; i<system.size(); i++){
       system[i].linked_to_index = find_planet_index( system, system[i].linked_to );
    }
 
    int links = 0;
-   for (int i=0; i<system.size(); i++){
+   for (size_t i=0; i<system.size(); i++){
 
       orbit_t planet = system[i];
       while (planet.linked_to != "COM"){
@@ -56,14 +56,14 @@ int main(){
       links += system[i].links;
    }
 
-   std::cout << "Number of links = " << links << std::endl;
+   std::cout << "Answer (part 1): " << links << std::endl;
 
    int YOU_index = find_planet_index( system, "YOU");
    int SAN_index = find_planet_index( system, "SAN");
 
    std::string join_at;
-   for (int i=0; i<system[YOU_index].path_to_COM.size(); i++){
-      for (int j=0; j<system[SAN_index].path_to_COM.size(); j++){
+   for (size_t i=0; i<system[YOU_index].path_to_COM.size(); i++){
+      for (size_t j=0; j<system[SAN_index].path_to_COM.size(); j++){
 
          if ( system[YOU_index].path_to_COM[i] == system[SAN_index].path_to_COM[j] ){
             join_at = system[YOU_index].path_to_COM[i];
@@ -76,11 +76,11 @@ int main(){
    int join_at_index = find_planet_index( system, join_at);
 
    int YOU_to_SAN = system[YOU_index].links + system[SAN_index].links - 2*system[join_at_index].links - 2;
-   std::cout << "Number of links from YOU to SAN: " << YOU_to_SAN << std::endl;
+   std::cout << "Answer (part 2): " << YOU_to_SAN << std::endl;
 
-   std::cout << system[YOU_index].links << std::endl;
-   std::cout << system[SAN_index].links << std::endl;
-   std::cout << system[join_at_index].links << std::endl;
+   // std::cout << system[YOU_index].links << std::endl;
+   // std::cout << system[SAN_index].links << std::endl;
+   // std::cout << system[join_at_index].links << std::endl;
 
    return 0;
 }
@@ -91,30 +91,4 @@ int find_planet_index( std::vector<orbit_t> system, std::string name){
    });
 
    return std::distance(system.begin(), it);
-}
-
-std::vector<std::string> read_input(){
-   // map of orbits
-   std::vector<std::string> map;
-
-   // open input file
-   std::ifstream myfile ("input");
-   if ( !(myfile.is_open()) ){
-      std::cerr << "Error:could not open input" << std::endl;
-      std::exit(EXIT_FAILURE);
-   }
-
-   // read orbits into map
-   std::string line;
-
-   // std::getline(myfile,line);
-   // map.push_back(line.substr(0,3));
-   // map.push_back(line.substr(4,1));
-
-   while (std::getline(myfile, line)){
-      map.push_back(line.substr(0,3));
-      map.push_back(line.substr(4,3));
-   }
-
-   return map;
 }
