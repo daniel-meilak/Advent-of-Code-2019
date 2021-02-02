2c3b6ed21b3cc8b1f99a3cd56018145f62ef2f86
Daniel <daniel.mlak@gmail.com>
2021-02-02 20:19:30 +0100

    Finished day 25

---
+++ week_4/day_25/day_25.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<string>
+#include<algorithm>
+#include<cstdlib>
+#include"../../Utils/utils.h"
+#include"intcode.h"
+
+std::list<long long> to_ascii(const std::string &command);
+void display_output(const std::vector<long long> &output);
+
+int main(){
+
+    // read input into vector of strings.
+    std::vector<long long> input = input_to_llint(read_input("input", ","));
+
+    computer_t comp(input,{},2);
+    comp.run();
+
+    std::string in;
+    while(true){
+        display_output(comp.output);
+        std::getline(std::cin,in);
+        comp.input = to_ascii(in);
+        comp.run();
+    }
+
+    std::cout << "Answer (part 1): " << std::endl;
+    display_output(comp.output);
+
+    return 0;
+}
+
+std::list<long long> to_ascii(const std::string &command){
+
+    std::list<long long> output;
+
+    for (const char &c : command){
+        output.push_back(c);
+    }
+    output.push_back('\n');
+
+    return output;
+}
+
+void display_output(const std::vector<long long> &output){
+
+    std::string s;
+
+    for (auto it=output.rbegin()+1; it!=output.rend(); it++){
+        s += *it;
+    }
+    std::reverse(s.begin(), s.end());
+
+    std::cout << s << std::endl;
+}
