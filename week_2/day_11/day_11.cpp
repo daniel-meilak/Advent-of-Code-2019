03bd8bd1e54c3096995622ba227c73db8259d17d
Daniel Meilak <daniel.mlak@gmail.com>
2021-12-31 03:49:55 +0100

    day 11 now shows correct answer for part 1

--- week_2/day_11/day_11.cpp
+++ week_2/day_11/day_11.cpp
@@ -4,7 +4,11 @@
 #include"intcode.h"
 #include"../../Utils/utils.h"
 
+// directions up,right,left,down
+const std::vector<std::vector<int>> direction = {{0,1},{1,0},{0,-1},{-1,0}};
+
 bool check_position(std::vector<std::vector<int>> positions, std::vector<int> pos);
+std::vector<std::vector<int>> run_comp(computer_t &comp, std::vector<std::vector<bool>>& grid, std::vector<int> pos);
 
 int main(){
 
@@ -12,24 +16,50 @@ int main(){
    std::vector<long long> input = input_to_int<long long>(read_input("input_11", ","));
 
    // grid of black/white, initialised as black
-   std::vector<std::vector<bool>> grid(100, std::vector<bool> (100, false));
-
-   // directions up,right,left,down
-   std::vector<std::vector<int>> direction = {{0,1},{1,0},{0,-1},{-1,0}};
+   std::vector<std::vector<bool>> grid1(150, std::vector<bool> (150, false));
+   std::vector<std::vector<bool>> grid2 = grid1;
 
    // robot position
-   std::vector<int> pos = {50,50};
+   std::vector<int> pos = {75,75};
 
-   // set starting grid point to white
-   grid[pos[0]][pos[1]] = true;
+   // set starting positions to black and while
+   grid1[pos[0]][pos[1]] = false;
+   grid2[pos[0]][pos[1]] = true;
+
+   computer_t comp1(input, {}, true);
+   computer_t comp2(input, {}, true);
+
+   std::vector<std::vector<int>> positions1, positions2;
+   positions1 = run_comp(comp1, grid1, pos);
+   positions2 = run_comp(comp2, grid2, pos);
+
+   std::cout << "Answer (part 1): " << positions1.size() << std::endl;
+   std::cout << "Answer (part 2): " << std::endl;
+
+   // print grid
+   // for (int i=grid2.size()-75; i>=70; i--){
+   //    for (size_t j=75; j<grid2.size(); j++){
+   //       if (grid2[j][i]==0){
+   //          std::cout << " ";
+   //       }
+   //       else std::cout << "0";
+   //    }
+   //    std::cout << std::endl;
+   // }
+   display(grid2);
+}
+
+bool check_position(std::vector<std::vector<int>> positions, std::vector<int> pos){
+
+   return std::find(positions.begin(), positions.end(), pos) != positions.end();
+}
+
+std::vector<std::vector<int>> run_comp(computer_t &comp, std::vector<std::vector<bool>>& grid, std::vector<int> pos){
 
    // robot direction
    int dir = 0;
 
-   // vector of positions accessed
-   std::vector<std::vector<int>> positions = {{50,50}};
-
-   computer_t comp(input, {}, true);
+   std::vector<std::vector<int>> positions = {pos};
 
    while (comp.status != "finished"){
 
@@ -60,23 +90,5 @@ int main(){
       if ( !check_position(positions, pos) ) positions.push_back(pos);
    }
 
-   std::cout << "Answer (part 1): " << positions.size() << std::endl;
-   std::cout << "Answer (part 2): " << std::endl;
-
-
-   // print grid
-   for (int i=grid.size()-50; i>=45; i--){
-      for (size_t j=50; j<grid.size(); j++){
-         if (grid[j][i]==0){
-            std::cout << " ";
-         }
-         else std::cout << "0";
-      }
-      std::cout << std::endl;
-   }
-}
-
-bool check_position(std::vector<std::vector<int>> positions, std::vector<int> pos){
-
-   return std::find(positions.begin(), positions.end(), pos) != positions.end();
+   return positions;
 }
